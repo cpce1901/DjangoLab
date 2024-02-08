22053a56ed0f3070f9a3da44a86bfb56f2118f6a
cpce.1901 <ircd.claudio@gmail.com>
2024-02-08 12:47:07 -0300

    ModifyLocalHost

--- appLab/apps/attendance/views.py
+++ appLab/apps/attendance/views.py
@@ -23,17 +23,20 @@ class StudentFoundFormView(FormView):
             if latest_attendance:
                 when_in_time = latest_attendance.date_in
                 time_inside_time = latest_attendance.time_inside
-                time_now = datetime.now().time()  
+                time_now = datetime.now()               
 
                 # Convert when_in_time.time() to datetime.timedelta
                 when_in_time_timedelta = timedelta(hours=when_in_time.hour, minutes=when_in_time.minute, seconds=when_in_time.second)
                 time_inside_time_timedelta = timedelta(hours=time_inside_time.hour, minutes=time_inside_time.minute, seconds=time_inside_time.second)
                 time_now_delta = timedelta(hours=time_now.hour, minutes=time_now.minute, seconds=time_now.second)
 
+                # Tiempo y Fecha de entrada + tiempo solicitado
+                time_start_inside = when_in_time + time_inside_time_timedelta
+
                 # Tiempo de entrada + tiempo solicitado
                 total_time = when_in_time_timedelta + time_inside_time_timedelta
                             
-                if time_now_delta > total_time:
+                if time_now > time_start_inside:
                     return redirect(reverse_lazy("attendance_app:attendance", kwargs={'student': id_student}))
                 else:            
                     remaining_time = total_time - time_now_delta
@@ -65,7 +68,7 @@ class AttendanceFormView(FormView):
         student_id = self.kwargs['student']
         time_inside = form.cleaned_data["time_inside"]
 
-        if time_inside == 0:
+        if time_inside <= 0 or time_inside > 3:
             form.add_error(None, "Debes seleccionar un tiempo valido de estadia...")
             return self.form_invalid(form)
         
@@ -79,5 +82,7 @@ class AttendanceFormView(FormView):
             attendance.save()
 
             return redirect(reverse_lazy("attendance_app:attendance", kwargs={'student': student_id}) + "?ok")
+        
+        
 
     
