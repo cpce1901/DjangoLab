b2b0387ac5854d0ef6c55c16b6234260b5597b46
cpce.1901 <ircd.claudio@gmail.com>
2024-03-18 11:48:31 -0300

    addNewDB

--- appLab/apps/attendance/admin.py
+++ appLab/apps/attendance/admin.py
@@ -5,7 +5,17 @@ from import_export.widgets import ManyToManyWidget, ForeignKeyWidget
 from import_export.admin import ExportActionModelAdmin, ImportExportModelAdmin
 from .models import Attendance, Students, Classes, Teams, Schools
 from .models import Students, Teams, Classes
-from django.db import models
+
+
+"""team_name = Field(column_name='students', attribute='students',widget=ManyToManyWidget(Students, field='id', separator=','))
+class_name = Field(column_name='class_name', attribute='class_name', widget=ForeignKeyWidget(Classes, field='id'))
+
+    def before_import_row(self, row, **kwargs):
+        team_id = row['id']
+        students_ids = [int(sid) for sid in row.get("students", "").split(",") if sid.strip()]
+        instance, created = Teams.objects.get_or_create(id=team_id, defaults={'id': team_id})
+        if created:
+            instance.students.add(*students_ids)"""
 
 # Resources
 class AttendanceResource(ModelResource):
@@ -30,23 +40,23 @@ class SchoolsResource(ModelResource):
 
 
 class StudentsResource(ModelResource):
+    class_name = Field(column_name='class_name', attribute='class_name',widget=ManyToManyWidget(Classes, field='id', separator=','))
+
     class Meta:
         model = Students
-        use_bulk = True
+        se_bulk = True
         batch_size = 500
 
 
-class TeamsResource(ModelResource):
-    students = Field(column_name='students', attribute='students',widget=ManyToManyWidget(Students, field='id', separator=','))
-    class_name = Field(column_name='class_name', attribute='class_name', widget=ForeignKeyWidget(Classes, field='id'))
+class TeamsResource(ModelResource):   
+    team_name = Field(column_name='team_name', attribute='team_name',widget=ManyToManyWidget(Students, field='id', separator=','))
 
     def before_import_row(self, row, **kwargs):
         team_id = row['id']
-        students_ids = [int(sid) for sid in row.get("students", "").split(",") if sid.strip()]
+        team_ids = [int(sid) for sid in row.get("team_name", "").split(",") if sid.strip()]
         instance, created = Teams.objects.get_or_create(id=team_id, defaults={'id': team_id})
         if created:
-            instance.students.add(*students_ids)
- 
+            instance.team_name.add(*team_ids)
 
     class Meta:
         model = Teams
@@ -60,7 +70,7 @@ class ClassesIline(admin.TabularInline):
 
 
 class TeamsInline(admin.TabularInline):
-    filter_vertical = ('students',)
+    filter_vertical = ('team_name',)
     model = Teams
     extra = 0
 
@@ -92,139 +102,134 @@ class ClassesAdmin(ImportExportModelAdmin, ExportActionModelAdmin):
     '''
 
     resource_class = ClassesResource
-    list_display = ('display_school_code', 'stage', 'year', 'code', 'name','teacher', 'display_teams')
-    search_fields = ('school__name', 'year')
-    list_filter = ('year', 'stage', 'code')
     inlines = (TeamsInline, )
+    list_display = ('display_school_code', 'stage', 'year', 'code', 'name', 'teacher', 'display_teams')
+    list_filter = ('year', 'stage', 'school__code')
 
-    @admin.display(description='Codigo')
+    @admin.display(description='Escuela')
     def display_school_code(self, obj):
         return obj.school.code
-
+    
+    @admin.display(description='Escuela')
+    def display_school(self, obj):
+        return obj.school.name
+    
+    @admin.display(description='Grupos')
     def display_teams(self, obj):
-        teams = obj.class_name.all()
+        teams = obj.class_team.all()
+
         if teams:
-            output = "<ul>"
+            output = '<ul>'
             for team in teams:
-                output += f"<li>{team.name}</li>"
-            output += "</ul>"
+                output += f'<li>{team.name}</li>'
+            output += '</ul>'
             return format_html(output)
         else:
-            return "No hay equipos asociados"
-        
-    display_teams.short_description = 'Equipos'
-
-
+            return ''
+    
+    
 @admin.register(Teams)
 class TeamsAdmin(ImportExportModelAdmin, ExportActionModelAdmin):
     resource_class = TeamsResource    
-    list_display = ('name', 'get_class', 'get_year', 'get_stage', 'get_students') 
+    list_display = ('name', 'display_class_name', 'display_class_name_year', 'display_class_name_stage', 'display_students')
     list_filter = ('class_name__school__code', 'class_name__year', 'class_name__stage')
-    filter_horizontal = ('students', )
-    search_fields = ('name', 'class_name__name', 'class_name__year')
+    filter_vertical = ('team_name',)
 
-    def get_students(self, obj):
-        students = obj.students.all()
+    @admin.display(description='Asignatura')
+    def display_class_name(self, obj):
+        return obj.class_name.name
+    
+    @admin.display(description='Año')
+    def display_class_name_year(self, obj):
+        return obj.class_name.year
+    
+    @admin.display(description='Semestre')
+    def display_class_name_stage(self, obj):
+        return obj.class_name.get_stage_display()
+    
+    @admin.display(description='Integrantes')
+    def display_students(self, obj):
+        students = obj.team_name.all()
+        
         if students:
-            output = "<ul>"
+            output = '<ul>'
             for student in students:
-                output += f"<li>{student.name} { student.last_name }</li>"
-            output += "</ul>"
+                output += f'<li>{student.name} {student.last_name}</li>'
+            output += '</ul>'
             return format_html(output)
         else:
-            return "No hay alumnos asociados"
-        
-    def get_class(self, obj):
-        class_name = obj.class_name.name if obj.class_name else None
-        return class_name
-    
-    def get_year(self, obj):
-        class_year = obj.class_name.year if obj.class_name else None
-        return class_year
-    
-    def get_stage(self, obj):
-        class_stage = obj.class_name.get_stage_display() if obj.class_name else None
-        return f'{class_stage} Semestre'
-    
-    
-    get_class.short_description = "Asignatura"
-    get_year.short_description = "Año"
-    get_stage.short_description = "Semestre"
-    get_students.short_description = 'Integrantes'
-
+            return ''
+   
 
 @admin.register(Students) 
 class StudentsAdmin(ImportExportModelAdmin, ExportActionModelAdmin): 
     resource_class = StudentsResource
-    list_display = ('display_full_name', 'rut', 'email', 'display_code', 'display_class', 'display_year', 'display_stage', 'display_team')
-    list_filter = ('students_team__class_name__school__code', 'students_team__class_name__code')
-    search_fields = ('name', 'last_name', 'rut', 'email')
-    ordering = ('name', )
-   
+    list_display = ('display_full_name', 'rut', 'email', 'display_class_name', 'display_class_year', 'display_class_stage', 'display_team')
+    list_filter = ('class_name__school__code', 'class_name__code')
+    search_fields = ('name', 'last_name')
+    filter_vertical = ('class_name',)
+
     @admin.display(description='Nombre completo')
     def display_full_name(self, obj):
-        if obj:
-            return f'{obj.name} {obj.last_name}'
-        return '-'
+        return f'{obj.name} {obj.last_name}'
     
-    @admin.display(description='Grupo')
-    def display_team(self, obj):
-        equipos_del_estudiante = obj.students_team.all()
-        nombres_asignaturas = []
-        for equipo in equipos_del_estudiante:
-            nombres_asignaturas.append(equipo.name)
-        return ", ".join(nombres_asignaturas)
-    
-
     @admin.display(description='Asignatura')
-    def display_class(self, obj):
-        equipos_del_estudiante = obj.students_team.all()
-        nombres_asignaturas = []
-        for equipo in equipos_del_estudiante:
-            nombres_asignaturas.append(equipo.class_name.name)
-        return ", ".join(nombres_asignaturas)
+    def display_class_name(self, obj):
+        classes = obj.class_name.all()
+
+        if classes:
+            output = '<ul>'
+            for class_name in classes:
+                output += f'<li>{class_name.name}</li>'
+            output += '</ul>'
+            return format_html(output)
+        else:
+            return ''
         
-    
     @admin.display(description='Año')
-    def display_year(self, obj):
-        equipos_del_estudiante = obj.students_team.all()
-        nombres_asignaturas = []
-        for equipo in equipos_del_estudiante:
-            nombres_asignaturas.append(str(equipo.class_name.year))
-        return ", ".join(nombres_asignaturas)
-        
+    def display_class_year(self, obj):
+        classes = obj.class_name.all()
+
+        if classes:
+            output = '<ul>'
+            for class_name in classes:
+                output += f'<li>{class_name.year}</li>'
+            output += '</ul>'
+            return format_html(output)
+        else:
+            return ''
     
     @admin.display(description='Semestre')
-    def display_stage(self, obj):
-        equipos_del_estudiante = obj.students_team.all()
-        nombres_asignaturas = []
-        for equipo in equipos_del_estudiante:
-            nombres_asignaturas.append(equipo.class_name.get_stage_display())
-        return ", ".join(nombres_asignaturas)
+    def display_class_stage(self, obj):
+        classes = obj.class_name.all()
+
+        if classes:
+            output = '<ul>'
+            for class_name in classes:
+                output += f'<li>{class_name.get_stage_display()}</li>'
+            output += '</ul>'
+            return format_html(output)
+        else:
+            return ''
+
+    @admin.display(description='Grupo')
+    def display_team(self, obj):
+        teams = obj.team_student.all()
+
+        if teams:
+            output = '<ul>'
+            for team in teams:
+                output += f'<li>{team.name}</li>'
+            output += '</ul>'
+            return format_html(output)
+        else:
+            return ''
     
-    @admin.display(description='Code')
-    def display_code(self, obj):
-        equipos_del_estudiante = obj.students_team.all()
-        nombres_asignaturas = []
-        for equipo in equipos_del_estudiante:
-            nombres_asignaturas.append(equipo.class_name.code)
-        return ", ".join(nombres_asignaturas)
-       
 
 @admin.register(Attendance)
 class AttendanceAdmin(ImportExportModelAdmin, ExportActionModelAdmin):
     resource_class = AttendanceResource
-    list_display = ('student', 'date_in', 'time_inside')
-    list_filter = (
-        'student__students_team__class_name__school__code', 
-        'student__students_team__class_name__name', 
-        'student__students_team__class_name__year', 
-        'student__students_team__class_name__stage'
-        )
-    search_fields =('student__name', 'student__last_name')
-
-    def student(self, obj):
-        return obj
+   
 
    
 
--- appLab/apps/attendance/models.py
+++ appLab/apps/attendance/models.py
@@ -26,27 +26,6 @@ class Schools(models.Model):
         return f"{self.name}"
 
 
-class Students(models.Model):
-
-    '''
-    Modelo de estudiante
-
-    '''
-     
-    name = models.CharField('Nombre', max_length=32)
-    last_name = models.CharField('Apellido', max_length=32)
-    rut = models.CharField('RUT', max_length=10, null=True, blank=True)
-    email = models.EmailField('Email', null=True)
-    
-    class Meta():
-        verbose_name = "Estudiante"
-        verbose_name_plural = "Estudiantes"
-        unique_together= ['name', 'last_name', 'rut']
-
-    def __str__(self):
-        return f"{self.name} {self.last_name}"
-
-
 class Classes(models.Model):
 
     '''
@@ -64,7 +43,7 @@ class Classes(models.Model):
     year = models.IntegerField('Año')
     stage = models.CharField('Semestre', max_length=1, choices=STAGE_CHOICES)
     teacher = models.CharField('Profesor', max_length=32, null=True, blank=True)
-    school = models.ForeignKey(Schools, on_delete=models.CASCADE, verbose_name='Carrera', related_name='school')
+    school = models.ForeignKey(Schools, on_delete=models.SET_NULL, verbose_name='Carrera', related_name='school', null=True, blank=True)
 
     class Meta():
         verbose_name = "Asignatura"
@@ -74,19 +53,41 @@ class Classes(models.Model):
         return f"{self.school.code} | {self.name} | {self.get_stage_display()} Semestre | {self.year}"
 
 
+class Students(models.Model):
+
+    '''
+    Modelo de estudiante
+
+    '''
+     
+    name = models.CharField('Nombre', max_length=32)
+    last_name = models.CharField('Apellido', max_length=32)
+    rut = models.CharField('RUT', max_length=10, null=True, blank=True)
+    email = models.EmailField('Email', null=True)
+    class_name = models.ManyToManyField(Classes, verbose_name='Asignatura', related_name='class_student', blank=True)
+    
+    class Meta():
+        verbose_name = "Estudiante"
+        verbose_name_plural = "Estudiantes"
+        unique_together= ['name', 'last_name', 'rut']
+
+    def __str__(self):
+        return f"{self.name} {self.last_name}"
+
+
 class Teams(models.Model):
     name = models.CharField('Nombre', max_length=32)
     challenge = models.CharField('Reto', max_length=32, null=True, blank=True)
-    class_name = models.ForeignKey(Classes, on_delete=models.CASCADE, verbose_name='Asignatura', related_name='class_name', null=True, blank=True)
-    students = models.ManyToManyField(Students, verbose_name='Estudiantes', related_name='students_team', blank=True)
-    
+    class_name = models.ForeignKey(Classes, on_delete=models.SET_NULL, verbose_name='Asignatura', related_name='class_team', null=True, blank=True)
+    team_name = models.ManyToManyField(Students, verbose_name='Equipo', related_name='team_student', blank=True)
+
     class Meta():
         verbose_name = "Equipo"
         verbose_name_plural = "Equipos"
         
     def __str__(self):
         return f"{self.name} | {self.class_name.name} | {self.class_name.year}"
-    
+
 
 class Attendance(models.Model):
     
